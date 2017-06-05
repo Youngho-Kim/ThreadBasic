86b8db7499446f66fafcf96f7f308eec57ca5994
KWAVE\kwave <kwave55@gmail.com>
2017-06-05 14:29:33 +0900

    thread start

---
+++ app/src/main/java/com/android/kwave/threadbasic/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.kwave.threadbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Thread 생성
+        Thread thread = new Thread();
+
+    }
+}
